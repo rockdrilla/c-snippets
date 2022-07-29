a8a3be73bd0e2732d2886b1bd1210e0e342eecdc
Konstantin Demin <rockdrilla@gmail.com>
2022-07-29 23:57:02 +0300

    initial
    import files from https://github.com/rockdrilla/ufind/tree/39d1e9257ef57cf302febc729e095eb9b5d3e465

---
+++ uhash/uhash.h
@@ -0,0 +1,22 @@
+/* uhash: simple grow-only hash
+ *
+ * - topmost header
+ *
+ * refs:
+ * - [1] https://github.com/etherealvisage/avl
+ * - [2] https://github.com/DanielGibson/Snippets
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * (c) 2022, Konstantin Demin
+ */
+
+#ifndef UHASH_H
+#define UHASH_H
+
+#include "uhash-common.h"
+#include "uhash-type0.h"
+#include "uhash-type1.h"
+#include "uhash-type2.h"
+#include "uhash-type3.h"
+
+#endif /* UHASH_H */
