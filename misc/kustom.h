83441aeb357412547ba0ae18f4be8d319ee4ea17
Konstantin Demin <rockdrilla@gmail.com>
2022-09-08 02:07:47 +0300

    misc/kustom.h: introduce

---
+++ misc/kustom.h
@@ -0,0 +1,20 @@
+/* kustom: shifty-nifty macros for custom names/types/funcs/etc
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * (c) 2022, Konstantin Demin
+ */
+
+#ifndef KUSTOM_H
+#define KUSTOM_H 1
+
+#define KUSTOM_NAME(prefix, type, kind)  prefix ## _ ## type ## _ ## kind
+
+#define KUSTOM_PROC(type, proc)       type ## __ ## proc
+#define KUSTOM_CALL(type, proc, ...)  KUSTOM_PROC(type, proc) (__VA_ARGS__)
+
+/* for "internals" */
+
+#define KUSTOM_PROC_INT(type, proc)       type ## __int__ ## proc
+#define KUSTOM_CALL_INT(type, proc, ...)  KUSTOM_PROC_INT(type, proc) (__VA_ARGS__)
+
+#endif /* KUSTOM_H */
