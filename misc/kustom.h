52a9a309f6382375c1723b0f9e7d1876198708e8
Konstantin Demin <rockdrilla@gmail.com>
2022-09-17 00:44:32 +0300

    misc/kustom.h: style/trivia

--- misc/kustom.h
+++ misc/kustom.h
@@ -10,11 +10,11 @@
 #define KUSTOM_NAME(prefix, type, kind)  prefix ## _ ## type ## _ ## kind
 
 #define KUSTOM_PROC(type, proc)       type ## __ ## proc
-#define KUSTOM_CALL(type, proc, ...)  KUSTOM_PROC(type, proc) (__VA_ARGS__)
+#define KUSTOM_CALL(type, proc, ...)  (KUSTOM_PROC(type, proc) (__VA_ARGS__))
 
 /* for "internals" */
 
 #define KUSTOM_PROC_INT(type, proc)       type ## __int__ ## proc
-#define KUSTOM_CALL_INT(type, proc, ...)  KUSTOM_PROC_INT(type, proc) (__VA_ARGS__)
+#define KUSTOM_CALL_INT(type, proc, ...)  (KUSTOM_PROC_INT(type, proc) (__VA_ARGS__))
 
 #endif /* HEADER_INCLUDED_KUSTOM */
