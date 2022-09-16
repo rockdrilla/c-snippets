9aebf26c0876fe71b1f14ee07802c19fbab65bc0
Konstantin Demin <rockdrilla@gmail.com>
2022-09-17 00:43:37 +0300

    uvector: refactor

--- uvector/common.h
+++ uvector/common.h
@@ -22,7 +22,6 @@
 #define _UVECTOR_DEFINE_CONSTANT(user_t, index_t, value_t) \
 	static const size_t UVECTOR_NAME(user_t, item_size)  = sizeof(value_t); \
 	static const size_t UVECTOR_NAME(user_t, align_size) = MEMFUN_MACRO_ALIGN(UVECTOR_NAME(user_t, item_size)); \
-	static const size_t UVECTOR_NAME(user_t, pad_size)   = UVECTOR_NAME(user_t, align_size) - UVECTOR_NAME(user_t, item_size); \
 	static const size_t UVECTOR_NAME(user_t, growth)     = MEMFUN_MACRO_CALC_GROWTH(UVECTOR_NAME(user_t, align_size)); \
 	static const int    UVECTOR_NAME(user_t, align_bits) = GETMSB_MACRO32(UVECTOR_NAME(user_t, item_size)); \
 	\
@@ -58,72 +57,58 @@
 	typedef void (* UVECTOR_NAME(user_t, cvisitor_ex)) (index_t index, const value_t * value, void * state); \
 
 
-#define _UVECTOR_PROC_INIT(user_t) \
+#define _UVECTOR_PROC__INIT_FREE(user_t, index_t) \
 	static void \
 	UVECTOR_PROC(user_t, init) (user_t * vector) { \
 		(void) memset(vector, 0, sizeof(user_t)); \
-	}
-
-
-#define _UVECTOR_PROCINT_OFFSET_OF(user_t, index_t) \
-	static CC_FORCE_INLINE size_t \
-	UVECTOR_PROC_INT(user_t, offset_of) (index_t index) { \
-		return (size_t) memfun_ptr_offset_ex(NULL, UVECTOR_NAME(user_t, align_size), index); \
-	}
-
-
-#define _UVECTOR_PROC_OFFSET_OF(user_t, index_t) \
-	static size_t \
-	UVECTOR_PROC(user_t, offset_of) (const user_t * vector, index_t index) { \
-		if (index >= vector->allocated) \
-			return UVECTOR_NAME(user_t, idx_inv); \
-		return UVECTOR_CALL_INT(user_t, offset_of, index); \
-	}
-
-
-#define _UVECTOR_PROCINT_PTR_OF(user_t, index_t, value_t) \
-	static CC_FORCE_INLINE value_t * \
-	UVECTOR_PROC_INT(user_t, ptr_of) (const user_t * vector, index_t index) { \
-		return (value_t *) memfun_ptr_offset_ex(vector->ptr, UVECTOR_NAME(user_t, align_size), index); \
-	}
-
-
-#define _UVECTOR_PROC_FREE(user_t) \
+	} \
+	\
+	static int \
+	UVECTOR_PROC(user_t, init_ex) (user_t * vector, index_t initial_size) { \
+		UVECTOR_CALL(user_t, init, vector); \
+		return UVECTOR_CALL(user_t, grow_by_count, vector, (initial_size) ? initial_size : 1); \
+	} \
+	\
 	static void \
 	UVECTOR_PROC(user_t, free) (user_t * vector) { \
-		memfun_free(vector->ptr, UVECTOR_CALL_INT(user_t, offset_of, vector->used)); \
+		memfun_free(vector->ptr, UVECTOR_CALL(user_t, offset_of, vector->used)); \
 		(void) memset(vector, 0, sizeof(user_t)); \
 	}
 
 
-#define _UVECTOR_PROCINT_IS_INV(user_t, index_t) \
+#define _UVECTOR_PROC__INDEX(user_t, index_t, value_t) \
+	static CC_FORCE_INLINE size_t \
+	UVECTOR_PROC(user_t, offset_of) (index_t index) { \
+		return (size_t) memfun_ptr_offset_ex(NULL, UVECTOR_NAME(user_t, align_size), index); \
+	} \
+	\
+	static CC_FORCE_INLINE value_t * \
+	UVECTOR_PROC_INT(user_t, ptr_of) (const user_t * vector, index_t index) { \
+		return (value_t *) memfun_ptr_offset_ex(vector->ptr, UVECTOR_NAME(user_t, align_size), index); \
+	} \
+	\
 	static CC_FORCE_INLINE int \
 	UVECTOR_PROC(user_t, is_inv) (index_t index) { \
 		return ((index & UVECTOR_NAME(user_t, idx_mask_inv)) != 0); \
-	}
-
-
-#define _UVECTOR_PROCINT_IS_WFALL(user_t, index_t) \
+	} \
+	\
 	static CC_FORCE_INLINE int \
 	UVECTOR_PROC_INT(user_t, is_wfall) (index_t index) { \
 		return ((index & UVECTOR_NAME(user_t, idx_mask_wfall)) != 0); \
 	}
 
-
-#define _UVECTOR_PROCINT_GROW_BY_BYTES(user_t) \
+#define _UVECTOR_PROC__GROW(user_t, index_t) \
 	static int \
 	UVECTOR_PROC_INT(user_t, grow_by_bytes) (user_t * vector, size_t bytes) { \
-		size_t _new = UVECTOR_CALL_INT(user_t, offset_of, vector->allocated); \
+		size_t _new = UVECTOR_CALL(user_t, offset_of, vector->allocated); \
 		void * nptr = memfun_realloc_ex(vector->ptr, &_new, bytes); \
 		if ((!nptr) || (!_new)) return 0; \
 		size_t _alloc = _new / UVECTOR_NAME(user_t, align_size); \
 		vector->allocated = (_alloc < UVECTOR_NAME(user_t, idx_max)) ? _alloc : UVECTOR_NAME(user_t, idx_max); \
 		vector->ptr = nptr; \
 		return 1; \
-	}
-
-
-#define _UVECTOR_PROCINT_GROW_BY_COUNT(user_t, index_t) \
+	} \
+	\
 	static int \
 	UVECTOR_PROC_INT(user_t, grow_by_count) (user_t * vector, index_t count) { \
 		size_t _new = 0; \
@@ -135,21 +120,17 @@
 		} \
 		if (UVECTOR_CALL(user_t, is_inv, _new)) \
 			return 0; \
-		return UVECTOR_CALL_INT(user_t, grow_by_bytes, vector, UVECTOR_CALL_INT(user_t, offset_of, count)); \
-	}
-
-
-#define _UVECTOR_PROC_GROW_BY_BYTES(user_t, index_t) \
+		return UVECTOR_CALL_INT(user_t, grow_by_bytes, vector, UVECTOR_CALL(user_t, offset_of, count)); \
+	} \
+	\
 	static int \
 	UVECTOR_PROC(user_t, grow_by_bytes) (user_t * vector, size_t bytes) { \
 		if (!bytes) return 0; \
 		if (vector->allocated >= UVECTOR_NAME(user_t, idx_max)) \
 			return 0; \
 		return UVECTOR_CALL_INT(user_t, grow_by_bytes, vector, bytes); \
-	}
-
-
-#define _UVECTOR_PROC_GROW_BY_COUNT(user_t, index_t) \
+	} \
+	\
 	static int \
 	UVECTOR_PROC(user_t, grow_by_count) (user_t * vector, index_t count) { \
 		if (!count) return 0; \
@@ -158,10 +139,7 @@
 		if (vector->allocated >= UVECTOR_NAME(user_t, idx_max)) \
 			return 0; \
 		return UVECTOR_CALL_INT(user_t, grow_by_count, vector, count); \
-	}
-
-
-#define _UVECTOR_PROC_GROW_AUTO(user_t) \
+	} \
 	static int \
 	UVECTOR_PROC(user_t, grow_auto) (user_t * vector) { \
 		if (vector->used < vector->allocated) \
@@ -170,36 +148,14 @@
 	}
 
 
-#define _UVECTOR_PROC_GET_BY_PTR(user_t, index_t, value_t) \
+#define _UVECTOR_PROC__BY_PTR(user_t, index_t, value_t) \
 	static value_t * \
 	UVECTOR_PROC(user_t, get_by_ptr) (const user_t * vector, index_t index) { \
 		if (index >= vector->used) \
 			return NULL; \
 		return UVECTOR_CALL_INT(user_t, ptr_of, vector, index); \
-	}
-
-
-#define _UVECTOR_PROCINT_GET_BY_VAL(user_t, index_t, value_t) \
-	static CC_FORCE_INLINE value_t \
-	UVECTOR_PROC_INT(user_t, get_by_val) (user_t * vector, index_t index) { \
-		value_t * item = UVECTOR_CALL_INT(user_t, ptr_of, vector, index); \
-		return *item; \
-	}
-
-
-#define _UVECTOR_PROC_GET_BY_VAL(user_t, index_t, value_t) \
-	static value_t \
-	UVECTOR_PROC(user_t, get_by_val) (user_t * vector, index_t index) { \
-		if (index >= vector->used) { \
-			value_t default_value; \
-			(void) memset(&default_value, 0, UVECTOR_NAME(user_t, item_size)); \
-			return default_value; \
-		} \
-		return UVECTOR_CALL_INT(user_t, get_by_val, vector, index); \
-	}
-
-
-#define _UVECTOR_PROCINT_SET_BY_PTR(user_t, index_t, value_t) \
+	} \
+	\
 	static CC_FORCE_INLINE void \
 	UVECTOR_PROC_INT(user_t, set_by_ptr) (user_t * vector, index_t index, value_t * source) { \
 		void * item = UVECTOR_CALL_INT(user_t, ptr_of, vector, index); \
@@ -207,39 +163,17 @@
 			(void) memcpy(item, source, UVECTOR_NAME(user_t, item_size)); \
 		else \
 			(void) memset(item, 0, UVECTOR_NAME(user_t, item_size)); \
-	}
-
-
-#define _UVECTOR_PROC_SET_BY_PTR(user_t, index_t, value_t) \
+	} \
+	\
 	static int \
 	UVECTOR_PROC(user_t, set_by_ptr) (user_t * vector, index_t index, value_t * source) { \
 		if (index >= vector->used) \
 			return 0; \
 		UVECTOR_CALL_INT(user_t, set_by_ptr, vector, index, source); \
 		return 1; \
-	}
-
-
-#define _UVECTOR_PROCINT_SET_BY_VAL(user_t, index_t, value_t) \
-	static CC_FORCE_INLINE void \
-	UVECTOR_PROC_INT(user_t, set_by_val) (user_t * vector, index_t index, value_t value) { \
-		value_t * item = UVECTOR_CALL_INT(user_t, ptr_of, vector, index); \
-		*item = value; \
-	}
-
-
-#define _UVECTOR_PROC_SET_BY_VAL(user_t, index_t, value_t) \
-	static int \
-	UVECTOR_PROC(user_t, set_by_val) (user_t * vector, index_t index, value_t value) { \
-		if (index >= vector->used) \
-			return 0; \
-		UVECTOR_CALL_INT(user_t, set_by_val, vector, index, value); \
-		return 1; \
-	}
-
-
-#define _UVECTOR_PROC_APPEND_BY_PTR(user_t, value_t) \
-	static int \
+	} \
+	\
+	static index_t \
 	UVECTOR_PROC(user_t, append_by_ptr) (user_t * vector, value_t * source) { \
 		if (!UVECTOR_CALL(user_t, grow_auto, vector)) \
 			return UVECTOR_NAME(user_t, idx_inv); \
@@ -248,8 +182,38 @@
 	}
 
 
-#define _UVECTOR_PROC_APPEND_BY_VAL(user_t, value_t) \
+#define _UVECTOR_PROC__BY_VAL(user_t, index_t, value_t) \
+	static CC_FORCE_INLINE value_t \
+	UVECTOR_PROC_INT(user_t, get_by_val) (user_t * vector, index_t index) { \
+		value_t * item = UVECTOR_CALL_INT(user_t, ptr_of, vector, index); \
+		return *item; \
+	} \
+	\
+	static value_t \
+	UVECTOR_PROC(user_t, get_by_val) (user_t * vector, index_t index) { \
+		if (index >= vector->used) { \
+			UVECTOR_NAME(user_t, val_t) default_value; \
+			(void) memset(&default_value, 0, UVECTOR_NAME(user_t, align_size)); \
+			return default_value._.dummy; \
+		} \
+		return UVECTOR_CALL_INT(user_t, get_by_val, vector, index); \
+	} \
+	\
+	static CC_FORCE_INLINE void \
+	UVECTOR_PROC_INT(user_t, set_by_val) (user_t * vector, index_t index, value_t value) { \
+		value_t * item = UVECTOR_CALL_INT(user_t, ptr_of, vector, index); \
+		*item = value; \
+	} \
+	\
 	static int \
+	UVECTOR_PROC(user_t, set_by_val) (user_t * vector, index_t index, value_t value) { \
+		if (index >= vector->used) \
+			return 0; \
+		UVECTOR_CALL_INT(user_t, set_by_val, vector, index, value); \
+		return 1; \
+	} \
+	\
+	static index_t \
 	UVECTOR_PROC(user_t, append_by_val) (user_t * vector, value_t value) { \
 		if (!UVECTOR_CALL(user_t, grow_auto, vector)) \
 			return UVECTOR_NAME(user_t, idx_inv); \
@@ -258,70 +222,56 @@
 	}
 
 
-#define _UVECTOR_PROC_WALK(user_t, index_t, value_t) \
+#define _UVECTOR_PROC__WALK(user_t, index_t, value_t) \
 	static void \
 	UVECTOR_PROC(user_t, walk) (user_t * vector, UVECTOR_NAME(user_t, visitor) visitor) { \
 		for (index_t i = 0; i < vector->used; i++) { \
 			visitor(i, UVECTOR_CALL_INT(user_t, ptr_of, vector, i)); \
 		} \
-	}
-
-
-#define _UVECTOR_PROC_WALK_EX(user_t, index_t, value_t) \
+	} \
+	\
 	static void \
 	UVECTOR_PROC(user_t, walk_ex) (user_t * vector, UVECTOR_NAME(user_t, visitor_ex) visitor, void * state) { \
 		for (index_t i = 0; i < vector->used; i++) { \
 			visitor(i, UVECTOR_CALL_INT(user_t, ptr_of, vector, i), state); \
 		} \
-	}
-
-
-#define _UVECTOR_PROC_CONST_WALK(user_t, index_t, value_t) \
+	} \
+	\
 	static void \
 	UVECTOR_PROC(user_t, const_walk) (const user_t * vector, UVECTOR_NAME(user_t, cvisitor) visitor) { \
 		for (index_t i = 0; i < vector->used; i++) { \
 			visitor(i, (const value_t *) UVECTOR_CALL_INT(user_t, ptr_of, vector, i)); \
 		} \
-	}
-
-
-#define _UVECTOR_PROC_CONST_WALK_EX(user_t, index_t, value_t) \
+	} \
+	\
 	static void \
 	UVECTOR_PROC(user_t, const_walk_ex) (const user_t * vector, UVECTOR_NAME(user_t, cvisitor_ex) visitor, void * state) { \
 		for (index_t i = 0; i < vector->used; i++) { \
 			visitor(i, (const value_t *) UVECTOR_CALL_INT(user_t, ptr_of, vector, i), state); \
 		} \
-	}
-
-
-#define _UVECTOR_PROC_RWALK(user_t, index_t, value_t) \
+	} \
+	\
 	static void \
 	UVECTOR_PROC(user_t, rwalk) (user_t * vector, UVECTOR_NAME(user_t, visitor) visitor) { \
 		for (index_t i = vector->used; (i--) != 0; ) { \
 			visitor(i, UVECTOR_CALL_INT(user_t, ptr_of, vector, i)); \
 		} \
-	}
-
-
-#define _UVECTOR_PROC_RWALK_EX(user_t, index_t, value_t) \
+	} \
+	\
 	static void \
 	UVECTOR_PROC(user_t, rwalk_ex) (user_t * vector, UVECTOR_NAME(user_t, visitor_ex) visitor, void * state) { \
 		for (index_t i = vector->used; (i--) != 0; ) { \
 			visitor(i, UVECTOR_CALL_INT(user_t, ptr_of, vector, i), state); \
 		} \
-	}
-
-
-#define _UVECTOR_PROC_CONST_RWALK(user_t, index_t, value_t) \
+	} \
+	\
 	static void \
 	UVECTOR_PROC(user_t, const_rwalk) (const user_t * vector, UVECTOR_NAME(user_t, cvisitor) visitor) { \
 		for (index_t i = vector->used; (i--) != 0; ) { \
 			visitor(i, (const value_t *) UVECTOR_CALL_INT(user_t, ptr_of, vector, i)); \
 		} \
-	}
-
-
-#define _UVECTOR_PROC_CONST_RWALK_EX(user_t, index_t, value_t) \
+	} \
+	\
 	static void \
 	UVECTOR_PROC(user_t, const_rwalk_ex) (const user_t * vector, UVECTOR_NAME(user_t, cvisitor_ex) visitor, void * state) { \
 		for (index_t i = vector->used; (i--) != 0; ) { \
--- uvector/type1.h
+++ uvector/type1.h
@@ -15,30 +15,11 @@
 	_UVECTOR_DEFINE_CONSTANT(user_t, index_t, value_t) \
 	_UVECTOR_DEFINE_TYPE(user_t, index_t, value_t) \
 	_UVECTOR_DEFINE_VISITOR_PROC(user_t, index_t, value_t) \
-	_UVECTOR_PROC_INIT(user_t) \
-	_UVECTOR_PROCINT_OFFSET_OF(user_t, index_t) \
-	_UVECTOR_PROC_OFFSET_OF(user_t, index_t) \
-	_UVECTOR_PROCINT_PTR_OF(user_t, index_t, value_t) \
-	_UVECTOR_PROC_FREE(user_t) \
-	_UVECTOR_PROCINT_IS_INV(user_t, index_t) \
-	_UVECTOR_PROCINT_IS_WFALL(user_t, index_t) \
-	_UVECTOR_PROCINT_GROW_BY_BYTES(user_t) \
-	_UVECTOR_PROCINT_GROW_BY_COUNT(user_t, index_t) \
-	_UVECTOR_PROC_GROW_BY_BYTES(user_t, index_t) \
-	_UVECTOR_PROC_GROW_BY_COUNT(user_t, index_t) \
-	_UVECTOR_PROC_GROW_AUTO(user_t) \
-	_UVECTOR_PROC_GET_BY_PTR(user_t, index_t, value_t) \
-	_UVECTOR_PROCINT_SET_BY_PTR(user_t, index_t, value_t) \
-	_UVECTOR_PROC_SET_BY_PTR(user_t, index_t, value_t) \
-	_UVECTOR_PROC_APPEND_BY_PTR(user_t, value_t) \
-	_UVECTOR_PROC_WALK(user_t, index_t, value_t) \
-	_UVECTOR_PROC_WALK_EX(user_t, index_t, value_t) \
-	_UVECTOR_PROC_CONST_WALK(user_t, index_t, value_t) \
-	_UVECTOR_PROC_CONST_WALK_EX(user_t, index_t, value_t) \
-	_UVECTOR_PROC_RWALK(user_t, index_t, value_t) \
-	_UVECTOR_PROC_RWALK_EX(user_t, index_t, value_t) \
-	_UVECTOR_PROC_CONST_RWALK(user_t, index_t, value_t) \
-	_UVECTOR_PROC_CONST_RWALK_EX(user_t, index_t, value_t) \
+	_UVECTOR_PROC__INDEX(user_t, index_t, value_t) \
+	_UVECTOR_PROC__GROW(user_t, index_t) \
+	_UVECTOR_PROC__INIT_FREE(user_t, index_t) \
+	_UVECTOR_PROC__WALK(user_t, index_t, value_t) \
+	_UVECTOR_PROC__BY_PTR(user_t, index_t, value_t) \
 
 
 #endif /* HEADER_INCLUDED_UVECTOR_TYPE1 */
