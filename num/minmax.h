1eba71d01c1dd34a90a41acca80d06396226fa91
Konstantin Demin <rockdrilla@gmail.com>
2022-09-29 01:33:05 +0300

    num/minmax.h: introduce

---
+++ num/minmax.h
@@ -0,0 +1,26 @@
+/* minmax: min/max
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * (c) 2022, Konstantin Demin
+ */
+
+#ifndef HEADER_INCLUDED_NUM_MINMAX
+#define HEADER_INCLUDED_NUM_MINMAX 1
+
+#ifndef min
+#define min(a,b) (((a) > (b)) ? (b) : (a))
+#endif
+
+#ifndef max
+#define max(a,b) (((a) > (b)) ? (a) : (b))
+#endif
+
+static
+int min_positive(int a, int b)
+{
+	if (a < 1) return max(b, 0);
+	if (b < 1) return max(a, 0);
+	return min(a, b);
+}
+
+#endif /* HEADER_INCLUDED_NUM_MINMAX */
