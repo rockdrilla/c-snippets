7aaefae114c0c87c76bd4bbf37720045c6a5ebe9
Konstantin Demin <rockdrilla@gmail.com>
2022-09-29 01:34:35 +0300

    io/const.h: introduce

---
+++ io/const.h
@@ -0,0 +1,20 @@
+/* const: redefine constants
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * (c) 2022, Konstantin Demin
+ */
+
+#ifndef HEADER_INCLUDED_IO_CONST
+#define HEADER_INCLUDED_IO_CONST 1
+
+#include <limits.h>
+
+#ifndef PATH_MAX
+#define PATH_MAX 4096
+#endif
+
+#ifndef FILENAME_MAX
+#define FILENAME_MAX 4096
+#endif
+
+#endif /* HEADER_INCLUDED_IO_CONST */
