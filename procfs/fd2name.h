9c8646ce70e5ea4e447291b8893c78c81802c590
Konstantin Demin <rockdrilla@gmail.com>
2022-09-29 01:35:39 +0300

    procfs/fd2name.h: introduce

---
+++ procfs/fd2name.h
@@ -0,0 +1,30 @@
+/* fd2name: (try) read real name from open file descriptor
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * (c) 2022, Konstantin Demin
+ */
+
+#ifndef HEADER_INCLUDED_PROCFS_FD2NAME
+#define HEADER_INCLUDED_PROCFS_FD2NAME 1
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+static uint32_t procfs_fd2name(int fd, char * buffer, uint32_t buffer_size)
+{
+	/* "/proc/self/fd/" - 14, "%d" - up to 10 */
+	char procfs_link[32];
+
+	if (fd < 0) return 0;
+
+	snprintf(procfs_link, sizeof(procfs_link), "/proc/self/fd/%d", fd);
+	ssize_t result = readlink(procfs_link, buffer, buffer_size - 1);
+	if (result <= 0) return 0;
+
+	return result;
+}
+
+#endif /* HEADER_INCLUDED_PROCFS_FD2NAME */
