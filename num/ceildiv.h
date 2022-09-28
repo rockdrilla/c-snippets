4e1c2f08b074592ad1cef85c16a584c54e9967bb
Konstantin Demin <rockdrilla@gmail.com>
2022-09-29 01:33:17 +0300

    num/ceildiv.h: introduce

---
+++ num/ceildiv.h
@@ -0,0 +1,24 @@
+/* ceildiv: (safe) "ceil-div" variant
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ * (c) 2022, Konstantin Demin
+ */
+
+#ifndef HEADER_INCLUDED_NUM_CEILDIV
+#define HEADER_INCLUDED_NUM_CEILDIV 1
+
+#include <stdlib.h>
+
+#include "../misc/cc-inline.h"
+
+#define _CEILDIV_DEFINE_FUNC(n, t) \
+	static CC_FORCE_INLINE \
+	t ceildiv ## n (t a, t b) { \
+		n ## div_t qr = n ## div(a, b); \
+		return qr.quot + ((qr.rem) ? 1 : 0); \
+	}
+_CEILDIV_DEFINE_FUNC(,   int)
+_CEILDIV_DEFINE_FUNC(l,  long)
+_CEILDIV_DEFINE_FUNC(ll, long long)
+
+#endif /* HEADER_INCLUDED_NUM_CEILDIV */
