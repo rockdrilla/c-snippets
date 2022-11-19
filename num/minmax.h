7e03a5865583b54372d0f10158fbe183b2daf586
Konstantin Demin <rockdrilla@gmail.com>
2022-11-19 15:18:30 +0300

    num/minmax.h: wrap macro

--- num/minmax.h
+++ num/minmax.h
@@ -17,13 +17,16 @@
 #define max(a,b) (((a) > (b)) ? (a) : (b))
 #endif
 
-static
-int min_positive(int a, int b)
-{
-	if (a < 1) return max(b, 0);
-	if (b < 1) return max(a, 0);
-	return min(a, b);
-}
+#define _MIN_POSITIVE_DEFINE_FUNC(n, t) \
+	static \
+	t min_positive ## n (t a, t b) { \
+		if (a < 1) return max(b, 0); \
+		if (b < 1) return max(a, 0); \
+		return min(a, b); \
+	}
+_MIN_POSITIVE_DEFINE_FUNC(,   int)
+_MIN_POSITIVE_DEFINE_FUNC(l,  long)
+_MIN_POSITIVE_DEFINE_FUNC(ll, long long)
 
 #include "../misc/ext-c-end.h"
 
