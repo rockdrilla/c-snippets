07eaaff2cb7d24d1ebd0839954ed953b67d43a75
Konstantin Demin <rockdrilla@gmail.com>
2022-11-19 21:26:08 +0300

    misc/memfun.h: formatting

--- misc/memfun.h
+++ misc/memfun.h
@@ -104,8 +104,8 @@ static const unsigned int memfun_growth_factor
 #define _MEMFUN_N2D_DUMB(x) \
 	( ((x)<<1) & ~((x)|((x)>>1)|((x)>>2)|((x)>>3)|((x)>>4)|((x)>>5)) )
 
-static CC_FORCE_INLINE
-size_t _memfun_n2d_dumb(size_t x)
+static
+CC_FORCE_INLINE size_t _memfun_n2d_dumb(size_t x)
 {
 	return (x<<1) & ~(x|(x>>1)|(x>>2)|(x>>3)|(x>>4)|(x>>5));
 }
@@ -135,8 +135,8 @@ size_t memfun_align(size_t length)
 	return _memfun_n2d_dumb(length);
 }
 
-static CC_FORCE_INLINE
-size_t memfun_block_align(size_t length)
+static
+CC_FORCE_INLINE size_t memfun_block_align(size_t length)
 {
 	if (!length) return 0;
 
@@ -288,8 +288,8 @@ void * memfun_realloc_ex(void * ptr, size_t * length, size_t extend)
 	return _memfun_realloc(ptr, _old, _new);
 }
 
-static CC_FORCE_INLINE
-void * memfun_realloc(void * ptr, size_t length, size_t extend)
+static
+CC_FORCE_INLINE void * memfun_realloc(void * ptr, size_t length, size_t extend)
 {
 	size_t old = length;
 	return memfun_realloc_ex(ptr, &old, extend);
